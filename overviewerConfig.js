7a9bebf8a92d24ae82e1ec625aeb59976070a0b9
david <bafadam@gmail.com>
2020-05-04 06:13:49 -0700

    deploying new map

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -28,7 +28,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1588594435",
+        "cacheTag": "1588598025",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -50,7 +50,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Shitty Hogwarts Overworld",
-            "last_rendertime": 1588594221,
+            "last_rendertime": 1588597821,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
@@ -77,7 +77,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Shitty Hogwarts Nether - nether",
-            "last_rendertime": 1588594221,
+            "last_rendertime": 1588597821,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
